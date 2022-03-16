fbdbd62e1e02c051192087cac22704870cf83916
Rishabh Dasgupta <rishabhdasu@gmail.com>
2022-03-16 23:56:44 +0530

    update

--- block-BNaaar/server.js
+++ block-BNaaar/server.js
@@ -2,8 +2,6 @@ var http = require("http");
 var server = http.createServer(handleReq);
 
 function handleReq(req, res) {
-  var parsedUrl = url.parse(req.url);
-  var pathname = parsedUrl.pathname;
   if (req.method === "GET" && req.url === "/") {
     res.write("Welcome to homepage");
     res.end();
@@ -13,8 +11,8 @@ function handleReq(req, res) {
   } else if (req.method === "POST" && req.url === "/about") {
     res.end("This is a post request");
   } else {
-    res.writeHead(404, { "Content-Type": "text/html" });
-    res.end("<h2>Page Not Found</h2>");
+    res.setHeader(404, { "Content-Type": "application/json" });
+    res.end(`{message: this is a post request}`);
   }
 }
 
